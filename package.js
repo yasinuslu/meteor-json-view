c57486bd09a9aa8159aa02a5646d07054d0e743a
Yasin Uslu <nepjua@gmail.com>
2015-10-16 07:59:40 +0300

    use 1.2.0.2 for ecmascript

--- package.js
+++ package.js
@@ -11,7 +11,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2');
+  api.versionsFrom('1.2.0.2');
   api.use('ecmascript');
   api.use(['jquery', 'templating'], 'client');
   api.addFiles('jquery.jsonview.css', 'client');
